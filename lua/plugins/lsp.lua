08ac6449718a5a4bbedfa40fa6558d70112350d6
asagao01F <daiki0214.fujii@icloud.com>
2026-06-13 03:18:04 +0900

    fix typo

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -21,7 +21,7 @@ return {
       keymap = { preset = "default" },
       appearance = {
         use_nvim_cmp_as_default = true,
-        nerd_fonts_variant = "mono",
+        nerd_font_variant = "mono",
       },
       sources = {
         default = { "lsp", "path", "snippets", "buffer" },
